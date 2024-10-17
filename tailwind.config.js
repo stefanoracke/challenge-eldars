82f1219a74325a06208dc7322a9057b890b5398b
stefanoracke <stefanoracke@gmail.com>
2024-10-17 20:12:00 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#4a8ecc",
+        dark: "#1A1A19"
+      }
+    },
+  },
+  plugins: [],
+}
+
